c62118ef2ce758b6a548403f894a00bbe746436a
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-11-23 10:10:02 -0700

    commit tree 6643fc24c2bfcaf243a1e7099eba7fbbfecfb408

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait
+
+
+
+    def clickbait
+        if title 
+            unless ["Won't Believe", "Secret", "Top [number]", "Guess"].any?{ |phrase| title.include? phrase }
+                errors.add(:title, "Title needs to be better" )
+            end
+        end
+    end
+
+
 end
